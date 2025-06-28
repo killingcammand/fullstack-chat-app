509f4994324c6b4a0531d3ad824b106a6c7a1efd
killingcammand <154828355+killingcammand@users.noreply.github.com>
2025-06-28 23:55:34 +0530

    dsvc

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: './',   // This fixes broken paths in index.html
+  build: {
+    outDir: 'dist'
+  }
 })
